5456d93dd30724b563481cee9832d5a6d240cde4
Saikat Bera <work.saikatbera@gmail.com>
2026-03-06 10:12:34 +0000

    use poppin font with uppercase (_for element h1_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Bitcount+Single:wght@100..900&family=Iceberg&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bitcount+Single:wght@100..900&family=Iceberg&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased min-h-screen selection:bg-primary/30" suppressHydrationWarning>
         <main>
