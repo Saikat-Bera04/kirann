afd504f23735df3053f58ad79216aca523ae645c
Saikat Bera <work.saikatbera@gmail.com>
2026-08-13 23:05:06 +0530

    chore: update project section background graphic to creative-design.png

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,7 +172,7 @@ export default function Home() {
             <Link href="/projects" className="block h-full min-h-[300px] md:min-h-0 group">
               <div className="glass-card h-full relative overflow-hidden flex flex-col justify-between group border-primary/10 hover:border-primary/40 transition-all rounded-[2rem]">
                 <Image 
-                  src="/graphics/developer-graphic.png" 
+                  src="/graphics/creative-design.png" 
                   alt="Projects" 
                   fill 
                   className="object-cover opacity-60 group-hover:opacity-80 group-hover:scale-105 transition-all duration-700" 
